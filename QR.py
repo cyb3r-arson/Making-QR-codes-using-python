5dbbd2ab640939bbb44a4d7a145132ca7353d5fc
Cyb3rarson <104206424+cyb3r-arson@users.noreply.github.com>
2023-04-21 02:07:47 -0400

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,1 +1,14 @@
+Making QR codes using Python.
+import qrcode
 
+
+text = "Hello, world!"
+
+
+qr = qrcode.QRCode(version=1, box_size=10, border=4)
+qr.add_data(text)
+qr.make(fit=True)
+
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("my_qr_code.png")
